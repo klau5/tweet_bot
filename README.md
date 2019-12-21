d5a0d74e928c59f4f72e66d4398cd5d6ec8f7e55
klau5 <jografie@gmail.com>
2019-12-21 08:47:35 +0530

    removed scheduler. added epub for bot to read from.

---
+++ README.md
@@ -0,0 +1,1 @@
+## A Twitter bot that tweets a line from an ebook every 15 minutes.
--- tweety.py
+++ tweety.py
@@ -1,16 +1,7 @@
 import tweepy
-import time
+from time import sleep
 from keys import *
 import schedule
-import json
-import requests
-
-# Advice API
-req = requests.get('https://api.adviceslip.com/advice', timeout=10)
-data = json.loads(req.text)
-data == req.json()
-advice_tweet = data['slip']['advice']
-print(advice_tweet)
 
 # Tweepy API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -18,18 +9,22 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 user = api.me()
 
-# post Tweet
-# tweet = api.update_status(advice_tweet)
+# book
+book = open('golden-hours.txt', 'r')
+lines = book.readlines()
+book.close()
 
-def post_tweet():
-    return advice_tweet
-print('\nReceived Quote\n')
-# print('\nTweet sent\n')
+def tweet():
+    for line in lines:
+        try:
+            print(line)
+            if line != '\n':
+                api.update_status(line) # tweet
+                sleep(900)
+            else:
+                pass
+        except tweepy.TweepError as err:
+            print(err.reason)
+            sleep(10)
 
-schedule.every(10).seconds.do(post_tweet)
-
-while 1:
-    t = time.ctime()
-    schedule.run_pending()
-    time.sleep(1)
-    print(f'Sleep Start {t}')
+tweet()
